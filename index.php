2dfbc5b97b4ee7e0c150e53fdf031c0df5d854e3
peter279k <peter279k@gmail.com>
2018-08-06 02:45:50 +0800

    Add send mail for the first page

--- index.php
+++ index.php
@@ -51,6 +51,28 @@ try {
         $msg .= 'Link      : ' . $media->getLink() . "<br/>";
         $msg .= '============================' . "<br/>";
         echo $msg;
+
+        // send email
+        $mail = new PHPMailer(true);
+        $mail->SMTPDebug = 0; // Set the SMTP Debug mode it can set 0, 1, 2
+        $mail->isSMTP();
+        $mail->Host = getenv('MAIL_HOST');  // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;        // Enable SMTP authentication
+        $mail->Username = getenv('MAIL_USERNAME');  // SMTP username
+        $mail->Password = getenv('MAIL_PASSWORD'); // SMTP password
+        $mail->SMTPSecure = 'tls';        // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587;
+        //Recipients
+        $mail->setFrom('admin@igclerks.com', 'Mailer');
+        $mail->addAddress('dsamsondeen@gmail.com', 'dsamsondeen');
+        //Content
+        $mail->isHTML(true); // Set email format to HTML
+        $mail->Subject = $media->getCaption();
+        $mail->Body    = $msg;
+        $mail->AltBody = $media->getLink();
+        $mail->send();
+        $checkPost->insertPostId($media->getId());
+
         break;
     }
 
